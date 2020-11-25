74b6f181247a68b7bb979fa4e6e0564863458a7c
shterneregen <shterneregen@gmail.com>
2020-11-25 22:08:55 +0300

    ignoring unknown field for objectMapper

--- src/main/java/random/telegramhomebot/TelegramHomeBotApplication.java
+++ src/main/java/random/telegramhomebot/TelegramHomeBotApplication.java
@@ -1,5 +1,6 @@
 package random.telegramhomebot;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,7 +14,9 @@ public class TelegramHomeBotApplication {
 
 	@Bean
 	ObjectMapper objectMapper() {
-		return new ObjectMapper();
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		return objectMapper;
 	}
 
 	public static void main(String[] args) {

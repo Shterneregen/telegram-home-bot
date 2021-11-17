f208d5f2a36d2313b716d25dfbaba9bac165ca66
shterneregen <shterneregen@gmail.com>
2021-11-18 00:58:51 +0300

    WakeOnLanService refactoring

--- src/main/java/random/telegramhomebot/services/WakeOnLanService.kt
+++ src/main/java/random/telegramhomebot/services/WakeOnLanService.kt
@@ -7,10 +7,17 @@ import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
 
+/**
+ * The magic packet is a frame that is most often sent as a broadcast and that contains anywhere within its payload
+ * 6 bytes of all 255 (FF FF FF FF FF FF in hexadecimal), followed by sixteen repetitions of the target computer's
+ * 48-bit MAC address, for a total of 102 bytes.
+ * */
 @Service
 class WakeOnLanService {
     val log = logger()
-    val macBytesSize = 6
+    val numbersOfBytesInMac = 6
+    val syncStreamSize = 6
+    val macDuplicationCount = 16
 
     @Value("\${wakeOnLan.port}")
     private lateinit var port: Number
@@ -21,35 +28,36 @@ class WakeOnLanService {
     fun wakeOnLan(mac: String?, ip: String = broadcastIp) {
         if (mac == null) return
         try {
-            val magicPacketBytes = getMagicPacketBytes(mac)
-            val socket = DatagramSocket()
-            socket.send(
-                DatagramPacket(magicPacketBytes, magicPacketBytes.size, InetAddress.getByName(ip), port.toInt())
-            )
-            socket.close()
+            DatagramSocket().run {
+                send(getMagicPacket(mac, ip))
+                close()
+            }
         } catch (e: Exception) {
             log.error("Failed to send Wake-on-LAN packet: {}", e.message)
             log.error(e.message, e)
         }
     }
 
+    private fun getMagicPacket(mac: String, ip: String) =
+        getMagicPacketBytes(mac).let { DatagramPacket(it, it.size, InetAddress.getByName(ip), port.toInt()) }
+
     private fun getMagicPacketBytes(mac: String): ByteArray {
         val macBytes = getMacBytes(mac)
-        val packetSize = macBytesSize + 16 * macBytes.size
-        val magicPacketBytes = ByteArray(packetSize)
-        for (i in 0 until macBytesSize) magicPacketBytes[i] = 0xff.toByte()
-        for (item in macBytesSize.until(magicPacketBytes.size).step(macBytes.size))
-            System.arraycopy(macBytes, 0, magicPacketBytes, item, macBytes.size)
-        return magicPacketBytes
+        return ByteArray(syncStreamSize + macDuplicationCount * macBytes.size).also { bytes ->
+            // The Synchronization Stream is defined as 6 bytes of FFh
+            for (i in 0 until syncStreamSize) bytes[i] = 0xff.toByte()
+            // The Target MAC block contains 16 duplications of the IEEE address of the target, with no breaks or interruptions
+            for (item in numbersOfBytesInMac.until(bytes.size).step(macBytes.size))
+                System.arraycopy(macBytes, 0, bytes, item, macBytes.size)
+        }
     }
 
     private fun getMacBytes(mac: String): ByteArray {
         val hex: List<String> = mac.split(":", "-")
-        if (hex.size != macBytesSize) throw IllegalArgumentException("Invalid MAC address")
+        if (hex.size != numbersOfBytesInMac) throw IllegalArgumentException("Invalid MAC address")
 
-        val bytes = ByteArray(macBytesSize)
-        for (i in 0 until macBytesSize) bytes[i] = hex[i].toInt(radix = 16).toByte()
-
-        return bytes
+        return ByteArray(numbersOfBytesInMac).also { bytes ->
+            for (i in 0 until numbersOfBytesInMac) bytes[i] = hex[i].toInt(radix = 16).toByte()
+        }
     }
 }
